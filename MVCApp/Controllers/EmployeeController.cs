57722603f015c68e16520c94542d672bfec00715
Kristian Babulev <kriso4os@abv.bg>
2018-11-25 21:18:54 +0200

    final

---
+++ MVCApp/Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCApp.Models;
+
+namespace MVCApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // Object reference to Staff class
+        private static Employee employee = new Employee();
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            // Get employee list from Model and send to View
+            return View(employee.employeeList);
+        }
+
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                EmployeeProperties employeeProperties = new EmployeeProperties();
+
+                employeeProperties.FirstName = collection["FirstName"];
+                employeeProperties.LastName = collection["LastName"];
+                employeeProperties.Address = collection["Address"];
+                employeeProperties.Email = collection["Email"];
+                employeeProperties.Salary = decimal.Parse(collection["Salary"]);
+
+                employee.addNewEmployee(employeeProperties);
+
+                return RedirectToAction("Index");
+            }
+
+            catch
+            {
+                return View();
+            }
+        }
+
+
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCApp/Models/EmployeeProperties.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class EmployeeProperties
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public decimal Salary { get; set; }
+    }
+    public class Employee
+    {
+        // Declare generic List data structure to hold staff details
+
+        public List<EmployeeProperties> employeeList = new List<EmployeeProperties>();
+
+        // Class constructor
+
+        public Employee()
+        {
+            employeeList.Add(new EmployeeProperties
+            {
+                FirstName = "Iliyan",
+                LastName = "Babulev",
+                Address = "Blagoevgrad",
+                Email = "liolio@abv.bg",
+                Salary = 20000
+
+            });
+            employeeList.Add(new EmployeeProperties
+            {
+                FirstName = "Hristina",
+                LastName = "Babuleva",
+                Address = "Blagoevgrad",
+                Email = "babuleva@abv.bg",
+                Salary = 135000
+
+            });
+            employeeList.Add(new EmployeeProperties
+            {
+                FirstName = "Mladen",
+                LastName = "Babulev",
+                Address = "Blagoevgrad",
+                Email = "babulev72@abv.bg",
+                Salary = 50000
+
+            });
+        }
+
+        // Method to add a new member of staff
+        public void addNewEmployee(EmployeeProperties newEmployee)
+        {
+            employeeList.Add(newEmployee);
+        }
+    }
+
+}
